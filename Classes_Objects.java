581b9e9eb1c48f8f1ce59cda30fd26cd3bfe13ed
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-10-28 12:39:28 +0530

    Classes Objects

--- Classes_Objects.java
+++ Classes_Objects.java
@@ -1,11 +1,38 @@
 package java_OOPs;
 
+/*
+ * OOPs -> (Object Oriented Programming System) It means programming using objects and classes.
+ *         In OOPs everything is represented as an object that has : Data(Called fields and variables)
+ *                                                                   Behavior 
+ */
+
 public class Classes_Objects {
 
 	public static void main(String[] args) {
 		
-		
+	/*  Encapsulation
+	it is use to achieve data security private access modifier is widely use in encapsulation
+	 * 	validation
+	 *  .contains
+	 *  CI and SI using program 
+	 */
 
 	}
 
+}
+
+
+class user{
+	private int key;
+	public void setKey(int key) {
+		this.key = key;
+	}
+	
+	public int getKey() {
+		return this.key;
+	}
+	
+	
+	
+	
 }
