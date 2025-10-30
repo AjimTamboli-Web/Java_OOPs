08da46d1515c5c39f338fb19f4a2c6b0fa1439fe
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-10-30 19:06:00 +0530

    Inheritance Types

---
+++ inheritance_types/Multilevel_Inheritance.java
@@ -0,0 +1,11 @@
+package java_OOPs.inheritance_types;
+
+public class Multilevel_Inheritance {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
---
+++ inheritance_types/Single_Inheritance.java
@@ -0,0 +1,10 @@
+package java_OOPs.inheritance_types;
+
+public class Single_Inheritance {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
