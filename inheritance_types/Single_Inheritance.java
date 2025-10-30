afdb8b46f21f2b668b3d84df339c379b5a632de4
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-10-30 19:24:21 +0530

    Single level inheritance

--- inheritance_types/Single_Inheritance.java
+++ inheritance_types/Single_Inheritance.java
@@ -1,10 +1,35 @@
 package java_OOPs.inheritance_types;
 
+/*
+ * Single Inheritance -> A sub-class is derived from only one super class.  
+ *  It inherits the properties and behavior of a single-parent class. Sometimes, it is also known as simple inheritance.
+ *  
+ */
+
 public class Single_Inheritance {
 
 	public static void main(String[] args) {
+	
+	// creating object of subclass invokes base class constructor.
 		
+		Car obj = new Car();
 
 	}
 
+}
+
+// Super class
+class Vehicle{
+	public Vehicle() {
+		
+		System.out.println("This is Vehicle.....");
+	}
+}
+
+// Sub class
+class Car extends Vehicle{
+	
+	public Car() {
+		System.out.println("This vehicle is car.");
+	}
 }
