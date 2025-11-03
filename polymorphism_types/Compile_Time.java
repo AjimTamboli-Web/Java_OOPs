127203812b6a6d7afe638e78a0fd080116ef3f0a
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-11-03 21:12:28 +0530

    Compile time Polymorphism

--- polymorphism_types/Compile_Time.java
+++ polymorphism_types/Compile_Time.java
@@ -1,5 +1,14 @@
 package java_OOPs.polymorphism_types;
 
+/*
+ *  Compile-Time Polymorphism ->It is implemented through Method overloading and Operator overloading. 
+ *   Compile-time polymorphism is also known as static polymorphism or early binding.
+ *   Compile-time polymorphism is a polymorphism that is resolved during the compilation process.
+ *   Overloading of methods is called through the reference variable of a class. 
+ *   Compile-time polymorphism is achieved by method overloading and operator overloading.
+ *  We can have one or more methods with the same name that are solely distinguishable by argument numbers, type, or order.
+ */
+
 public class Compile_Time {
 
 	public static void main(String[] args) {
