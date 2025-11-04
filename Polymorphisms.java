e7f296619bc7ee728e34c20c893b42c1ec722d07
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-11-04 18:21:33 +0530

    Intro Polymorphism

--- Polymorphisms.java
+++ Polymorphisms.java
@@ -3,11 +3,10 @@ package java_OOPs;
 /*
  * Polymorphism -> (Many forms) and it occurs when we have many classes that are related to each other by inheritance.
  * Inheritance lets us inherit attributes and methods from another class. Polymorphism uses those methods
- *              to perform a single action in different ways.   
+                                                                to perform a single action in different ways.   
  * In short a same thing can perform/ can have different implementation.
  *  - The ability of an object, method or operator to take on different behaviors based on context.
- 
- 	Why use? -> Code Reusability, Flexibility, Abstraction, Dynamic Behavior.
+ *  Why use? -> Code Reusability, Flexibility, Abstraction, Dynamic Behavior.
  
  *  Types of Polymorphism :-
  *  1) Compile-time/ static/ Early binding/ method overloading Polymorphism.
@@ -33,14 +32,14 @@ public class Polymorphisms {
 }
 
 class Book{
-	public void read() {
+	public void read() {         
 		System.out.println("Recommended Books for reading...");
 	}
-	
+	// Compile time Polymorphism
 	public void read(String name) {
 		System.out.println("This book written by ..." + name);
 	}
-	
+	// Compile time Polymorphism
 	public void read(int parts) {
 		System.out.println("Book of Series - " + parts);
 	}
@@ -50,6 +49,7 @@ class Got extends Book{
 	Got(){
 		System.out.println("Game of Thrones Based on ...");
 	}
+	// Run time Polymorphism
 	public void read() {
 		System.out.println("Song of Ice and Fire Book ");
 	}
@@ -59,6 +59,7 @@ class HotD extends Book{
 	HotD(){
 		System.out.println("House of the Dragons Based on....");
 	}
+	// Run time Polymorphism
 	public void read() {
 		System.out.println("Dance of  Dragons");
 	}
