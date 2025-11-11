1791216f48c568c67d9b6cd7291a1363d9378c60
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-11-11 06:05:29 +0530

    Abstraction Operations ans rules

--- Abstractions/Abstraction_details.java
+++ Abstractions/Abstraction_details.java
@@ -1,38 +1,55 @@
 package java_OOPs.Abstractions;
-
-public class Abstraction_details {
-
-	public static void main(String[] args) {
-		
-		Vehicle v1 = new Bike();
-		v1.start();
-		v1.stop();
-		Vehicle v2 = new Truck();
-		v2.start();
-		v2.stop();
-        v2.insurence();  // common behavior
-	}
-
-}
+/*
+ * Abstraction => 4th pillar of oops in java - it hides the complexity from the user as well as other parts of the program
+ *                            In the abstraction you only concern about what instead of how.
+ * Using abstract keyword you initialized abstract class or method. (its fixed the behavior) add functionality
+ * A method ends with the semicolon or incomplete method that is called abstract method.it does not have object,but constructors are allowed.
+ * THe method is need for overridden,and it set the compulsion.In abstract class not compulsion to have all abstract methods.  
+ * It may have both abstract and non-abstract methods(methods with bodies).
+ * only abstract method can be declare in abstract method.
+ * Private abstract method are not possible in class,only default,protected and public.
+ * We can define static methods in an abstract class.
+ * If a subclass does not implement all abstract methods of an abstract class, it must be declared abstract 
+ *                                                            itself, otherwise, a compile-time error occurs.
+ *                                                            
+ * Why? => Security - hides internal code logic, Flexibility, Maintainability, Polymorphism support - work with dynamic method dispatch
+ *         cleaner design - separates what to do from how to do. 
+ */
 
 abstract class Vehicle{
-	abstract void start();
+	private int wheels;  // it can be accept private variable only
+	
+	abstract void start();    // incomplete method
 	abstract void stop();
-	void insurence() {    //concrete method
+	
+	void insurence() {              //concrete method
 		 System.out.println("Get a Insurence");
 	 }
+	static void greet() {
+		System.out.println("This vehicle is ");
+	}
+	public void setWheel(int w) {
+		this.wheels = w;
+	}
+	public int getWheel() {
+		return this.wheels;
+	}
+      final protected static void license() {
+    	  System.out.println("License is compulsary to drive.");
+      }
 }
 
 class Bike extends Vehicle{
-	Bike(){
+	Bike(){           // default constructor for overridden child class
 		System.out.println("********** BIKE ***********");
 	}
-	 void start() { // implemented of abstract method
+	 void start() { 		// implemented of abstract method
 		System.out.println("Bike starts with a kick");
 	}
-	 void stop() {  // implemented of abstract method
+	 void stop() {  		// implemented of abstract method
 		System.out.println("Bike stop manually ");
 	}
+	
 }
 
 class Truck extends Vehicle{
@@ -46,3 +63,24 @@ class Truck extends Vehicle{
 		System.out.println("Truck stops with brakes");
 	}
 }
+
+public class Abstraction_details {
+
+	public static void main(String[] args) {
+		
+		Vehicle v1 = new Bike();
+		v1.start();
+		v1.stop();
+		v1.setWheel(2);
+		System.out.println("Bike can be number of wheel is " + v1.getWheel());
+		
+		Vehicle v2 = new Truck();
+		Vehicle.greet();  // static method
+		v2.start();
+		v2.stop();
+        v2.insurence();    // common behavior
+        Vehicle.license();  // final protected static method
+	}
+
+}
+
