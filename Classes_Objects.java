482d7dfa6df8a769227625944a26a3953a85c977
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-11-15 22:50:12 +0530

    Classes and Objects

--- Classes_Objects.java
+++ Classes_Objects.java
@@ -4,25 +4,24 @@ package java_OOPs;
  * OOPs -> (Object Oriented Programming System) It means programming using objects and classes.
  *         In OOPs everything is represented as an object that has : Data(Called fields and variables)
  *                                                                   Behavior 
+ * Object class -> Object classes is root class of the all classes in java every class has object as a superclass by default.
+ *                                                                                 
  */
 
 public class Classes_Objects {
 
 	public static void main(String[] args) {
 		
-	/*  Encapsulation
-	it is use to achieve data security private access modifier is widely use in encapsulation
-	 * 	validation
-	 *  .contains
-	 *  CI and SI using program 
-	 */
+	 User user = new User(); // this new keyword create object
+	 System.out.println(user);  // it prints hexa-cade address of store where object is.
 
 	}
 
 }
 
 
-class user{
+
+class User{
 	private int key;
 	public void setKey(int key) {
 		this.key = key;
